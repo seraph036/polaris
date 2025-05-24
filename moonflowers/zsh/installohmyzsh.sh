1335a5ff2bfb17ff82e43caabeaf52c62159db42
seraph036 <alexandre.hipolito@proton.me>
2025-05-24 10:57:11 -0300

    updated install script, added script for changing theme after install, modded theme configs

--- moonflowers/zsh/installohmyzsh.sh
+++ moonflowers/zsh/installohmyzsh.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/zsh
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
--- swap-theme.sh
+++ swap-theme.sh
@@ -12,26 +12,35 @@ esac
 }
 
 symlink_fix() {
-sudo rm folder-root.svg
+# Find all symlinks and process them
+#find . -maxdepth 1 -type l | while read -r link; do
+    # Get the target path
+    #target=$(readlink "$link")
+
+    # Create backup of original symlink
+    #cp -P "$link" "${link}.bak"
+
+    # Replace the text in the target path
+    #new_target=${target//theme/$chosenTheme}
+
+    # Remove old symlink and create new one
+    #unlink "$link"
+    #ln -s "$new_target" "$(basename "$link")"
+
+    #echo "Processed symlink: $link"
+#done
+sudo rm folder-root.svg &&
 sudo ln -s folder-$chosenTheme-private.svg folder-root.svg
 }
 
 install_config() {
 choose_theme
-# Test if necessary packages are installed
-if [ ! -e /usr/bin/git ] || [ ! -e /usr/bin/papirus-folders ] || [ ! -e /usr/bin/zsh ] || [ ! -e /usr/bin/fish ] || [ ! -d /usr/share/icons/Papirus ] || [ ! -e /usr/bin/starship ] || [ ! -e /usr/bin/fastfetch ] || [ ! -e /usr/bin/fzf ] || [ ! -e /usr/bin/eza ]; then
-echo "Some needed packages are not installed. Please install papirus-icon-theme, papirus-folder, fish, zsh, starship, fastfetch, fzf and eza then try again"; exit
-fi;
 echo "Copying files..." &&
-cp -rfv ./$chosenTheme/config/* ~/.config/ &&
-cp -rfv ./$chosenTheme/local/share/* ~/.local/share/ &&
-sudo cp -rfv ./fonts/* /usr/share/fonts &&
-sudo cp -rfv ./polaris-folders/* /usr/share/icons/Papirus &&
-echo "Running omz installation script"
-./$chosenTheme/zsh/installohmyzsh.sh;
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-cp -rfv $chosenTheme/zsh/.zshrc ~/.zshrc
+rm -rf ~/.config/{fastfetch,fish,kdeglobals,konsolerc,plasma-org.kde.plasma.desktop-appletsrc,starship.toml,Trolltech.conf}
+cp -rfv ./$chosenTheme/config/{fastfetch,fish,kdeglobals,konsolerc,plasma-org.kde.plasma.desktop-appletsrc,starship.toml,Trolltech.conf} ~/.config/ &&
+rm -rf ~/.local/share/{applications,color-schemes,konsole,theme-icons,wallpapers}
+cp -rfv ./$chosenTheme/local/share/{applications,color-schemes,konsole,theme-icons,wallpapers} ~/.local/share/ &&
+cp -rfv ./$chosenTheme/zsh/.zshrc ~/.zshrc &&
 echo "Applying colour theme to icons" &&
 cd /usr/share/icons/Papirus/ &&
 cd './22x22/places' &&
@@ -48,6 +57,7 @@ echo "Setting Papirus folder icon colours" &&
 papirus-folders -t Papirus-Dark -C $chosenTheme &&
 echo "All done. Now, head over to the Plasma settings and apply the theme colours, and the Papirus-Dark icon pack. Then, read the widget instructions and apply the theme there as well. Enjoy!"
 }
+
 # Start by backing up the current .config and .local/share directories, if they exist and the user wishes to do so
 read -p "Do you want to back up your .config and .local/share files? (might take a minute, recommended) (y/n)? " choice
 case "$choice" in
@@ -72,5 +82,3 @@ case "$choice" in
   n|N|no ) install_config;;
   * ) echo "Please answer y or n";;
 esac
-
-
